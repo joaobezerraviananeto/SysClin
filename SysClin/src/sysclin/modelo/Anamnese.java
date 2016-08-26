ff36bc624daa43b61be6e7d2e6414a180c3b9dc1
joaobezerraviananeto <joaobvn@gmail.com>
2016-08-26 11:26:20 -0300

    classes
    Adicionei a super classe atendimento e as sub classes anamnese e triagem

---
+++ SysClin/src/sysclin/modelo/Anamnese.java
@@ -0,0 +1,12 @@
+
+package sysclin.modelo;
+
+/**
+ *
+ * @author Pessoal
+ */
+public class Anamnese extends Atendimento {
+    private int id;
+    private String pergunta;
+    
+}
---
+++ SysClin/src/sysclin/modelo/Triagem.java
@@ -0,0 +1,12 @@
+
+package sysclin.modelo;
+
+/**
+ *
+ * @author Pessoal
+ */
+public class Triagem extends Atendimento{
+    private int id;
+    private int paciente = Paciente;
+    
+}
