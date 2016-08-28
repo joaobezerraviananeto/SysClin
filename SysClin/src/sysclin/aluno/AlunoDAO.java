e5bdb119f263c9c60afb6fc6db5d9e7c915a05b1
joaobezerraviananeto <joaobvn@gmail.com>
2016-08-28 16:34:05 -0300

    AlunoDAO
    Excluir aluno

--- SysClin/src/sysclin/aluno/AlunoDAO.java
+++ SysClin/src/sysclin/aluno/AlunoDAO.java
@@ -7,6 +7,7 @@ package sysclin.aluno;
 
 import java.util.List;
 import javax.swing.JOptionPane;
+import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import sysclin.util.HibernateUtil;
@@ -27,7 +28,7 @@ public class AlunoDAO {
             JOptionPane.showMessageDialog(null, "Ocorreu um erro ao tentar cadastrar o aluno", "Erro", JOptionPane.ERROR_MESSAGE);
         }
     }
-    
+
     public void alterar(Aluno aluno) {
         try {
             sessao = HibernateUtil.getSessionFactory().openSession();
@@ -39,8 +40,29 @@ public class AlunoDAO {
             JOptionPane.showMessageDialog(null, "Ocorreu um erro ao tentar alterar o aluno", "Erro", JOptionPane.ERROR_MESSAGE);
         }
     }
-    
-    public List<Aluno> buscarAlunos(){
-    
+
+    public void excluir(Aluno aluno) {
+        int resposta = JOptionPane.showConfirmDialog(null, "Tem certeza que deseja deletar este aluno?", "Certeza?", JOptionPane.YES_NO_OPTION);
+        if (resposta == JOptionPane.YES_OPTION) {
+            try {
+                sessao = HibernateUtil.getSessionFactory().openSession();
+                transacao = sessao.beginTransaction();
+                sessao.delete(aluno);
+                transacao.commit();
+                sessao.clear();
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, "Ocorreu um erro ao tentar deletar o aluno", "Erro", JOptionPane.ERROR_MESSAGE);
+            }
+        }
+    }
+
+    public List<Aluno> buscarAlunos() {
+        sessao = HibernateUtil.getSessionFactory().openSession();
+        transacao = sessao.beginTransaction();
+        Criteria criteriaAlunos = sessao.createCriteria(Aluno.class);
+        List<Aluno> alunos = criteriaAlunos.list();
+        sessao.clear();
+
+        return alunos;
     }
 }
---
+++ SysClin/src/sysclin/aluno/AlunoTableModel.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sysclin.aluno;
+
+import java.util.List;
+import javax.swing.table.AbstractTableModel;
+
+/**
+ *
+ * @author Joao_Viana
+ */
+public class AlunoTableModel extends AbstractTableModel {
+
+    public AlunoTableModel(List<Aluno> alunos) {
+        this.alunos = alunos;
+        this.tituloColuas = new String[]{"Nome", "Cpf", "Endereço"};
+    }
+
+    private List<Aluno> alunos;
+    private final String[] tituloColuas;
+
+    @Override
+    /*get row pega o numero de linhas*/
+    public int getRowCount() {
+        return getAlunos().size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return tituloColuas.length;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        Aluno aluno = getAlunos().get(rowIndex);
+
+        switch (columnIndex) {
+            case 0:
+                return aluno.getNome();
+            case 1:
+                return aluno.getCpf();
+            case 2:
+                return aluno.getEndereco();
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public String getColumnName(int column) {
+        // return tituloColuas[column];
+        switch (column) {
+            case 0:
+                return tituloColuas[0];
+            case 1:
+                return tituloColuas[1];
+            case 2:
+                return tituloColuas[2];
+            default:
+                return null;
+        }
+    }
+
+    public Aluno getRow(int row) {
+        return getAlunos().get(row);
+    }
+
+    /**
+     * @return the alunos
+     */
+    public List<Aluno> getAlunos() {
+        return alunos;
+    }
+
+    /**
+     * @param alunos the alunos to set
+     */
+    public void setAlunos(List<Aluno> alunos) {
+        this.alunos = alunos;
+        fireTableDataChanged();
+    }
+
+}
