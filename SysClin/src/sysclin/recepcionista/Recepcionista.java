2daa82bb94139b8dea0b72524bd08a61eebd5690
joaobezerraviananeto <joaobvn@gmail.com>
2016-09-04 16:43:26 -0300

    recepcionista table model
    hbhcdhch

--- SysClin/src/sysclin/recepcionista/Recepcionista.java
+++ SysClin/src/sysclin/recepcionista/Recepcionista.java
@@ -6,5 +6,14 @@ import sysclin.usuario.Usuario;
 
 @Entity
 public class Recepcionista extends Usuario{
+       private String turno;
+
+    public String getTurno() {
+        return turno;
+    }
+
+    public void setTurno(String turno) {
+        this.turno = turno;
+    }
        
 }
