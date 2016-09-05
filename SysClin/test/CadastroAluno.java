6cd5642493e65daeb2d517aa20ac39e599ed73e2
joaobezerraviananeto <joaobvn@gmail.com>
2016-09-05 11:04:57 -0300

    Telas
    deletadas e adicionadas

--- SysClin/test/CadastroAluno.java
+++ SysClin/test/CadastroAluno.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package sysclin.telas;
+
 
 import sysclin.aluno.Aluno;
 import sysclin.aluno.AlunoDAO;
